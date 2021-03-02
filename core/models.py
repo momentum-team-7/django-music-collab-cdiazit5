2d1eb98920ea7ca076b438aa708fbf89d359a2f1
cdiazit5 <cdiazit5@gmail.com>
2021-03-02 10:16:51 -0500

    got the FK working for artists, deleted data to make new model objs

--- core/models.py
+++ core/models.py
@@ -9,18 +9,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Album(models.Model):
-    title = models.CharField(max_length=280)
-    artist = models.CharField(max_length=75)
-    release_date = models.IntegerField(blank=True, null=True)
-    # one to many relationship, using the foreign key
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
-
-    # this change doesn't change the DB so no mirgrations are needed 
-    def __str__(self):
-        return self.title
-
-
 class Artist(models.Model):
     name = models.CharField(max_length=50)
     genre = models.CharField(max_length=100)
@@ -29,3 +17,12 @@ class Artist(models.Model):
     def __str__(self):
         return self.name
 
+class Album(models.Model):
+    title = models.CharField(max_length=280)
+    # artist = models.CharField(max_length=75)
+    release_date = models.IntegerField(blank=True, null=True)
+    # one to many relationship, using the foreign key
+    artist = models.ForeignKey(Artist, max_length=100, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
+    # this change doesn't change the DB so no mirgrations are needed 
+    def __str__(self):
+        return self.title
